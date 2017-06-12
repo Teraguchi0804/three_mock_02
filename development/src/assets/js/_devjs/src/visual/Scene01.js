8d37bcf8128f307b0ea23b1f0ce3ce434180582f
Norikazu Teraguchi <norimaki.tera@gmail.com>
2017-06-12 13:03:08 +0900

    シーン01：キューブローテーションを追加

--- development/src/assets/js/_devjs/src/visual/Scene01.js
+++ development/src/assets/js/_devjs/src/visual/Scene01.js
@@ -18,8 +18,13 @@ export default class Scene01 extends Entry{
 
     this.scene = null;
     this.camera = null;
-    this.Box = null;
+    // this.Box = null;
+    this.cube = null;
     this.timer = 0;
+
+    this.group = null;
+
+    this.render = this._render.bind(this);
 
     this.createScene = this._createScene.bind(this);
     this.update = this._update.bind(this);
@@ -37,17 +42,60 @@ export default class Scene01 extends Entry{
 
     this.scene = new THREE.Scene(); //シーン作成
 
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 10000);
+    this.group = new THREE.Group();
+    const step =30;
+    const num = 10;
+    for (let x = 1; x <= num; x++) {
+      for (let y = 1; y <= num; y++) {
+        for (let z = 1; z <= num; z ++) {
+          this.cube = new THREE.Mesh(
+              new THREE.BoxGeometry(10, 10, 10),
+              new THREE.MeshLambertMaterial(0xffffff)
+          );
+          this.cube.position.set(
+              (x - num / 2) * step,
+              (y - num / 2) * step,
+              (z - num / 2) * step
+          );
+          this.group.add(this.cube);
+        }
+      }
+    }
+    this.scene.add(this.group);
+
+    const pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(200, 200, 200);
+    this.scene.add(pointLight);
+
+    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000);
     this.camera.position.z = 1000;
 
-    this.Box = new THREE.Mesh(
-        new THREE.BoxGeometry(50,50,50),
-        new THREE.MeshBasicMaterial(0xffffff)
-    );
+    // this.Box = new THREE.Mesh(
+    //     new THREE.BoxGeometry(50,50,50),
+    //     new THREE.MeshBasicMaterial(0xffffff)
+    // );
 
-    this.scene.add(this.Box);
+    // this.scene.add(this.Box);
 
   }
+
+
+  _render() {
+    this.group.rotation.x += 0.01;
+    this.group.rotation.y += 0.01;
+    this.group.rotation.z += 0.01;
+    this.group.traverse(function(obj) {
+      if (obj != this.group) {
+        obj.rotation.x += 0.01;
+        obj.rotation.y += 0.01;
+        obj.rotation.z += 0.01;
+      }
+    }.bind(this));
+    // stats.update();
+    // renderer.render(scene, camera);
+    // requestAnimationFrame(render);
+  }
+
 
   /**
    *
@@ -55,8 +103,11 @@ export default class Scene01 extends Entry{
    */
   _update(){
 
-    this.timer += 0.1;
-    this.Box.position.x = 50 * Math.sin(this.timer);
+    // window.console.log('update01');
+
+    this.render();
+    // this.timer += 0.1;
+    // this.Box.position.x = 50 * Math.sin(this.timer);
 
   }
 
